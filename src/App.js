af526c1e515a04c59084a6e5d1bbdec4369b4e70
Paul <paulglujan@gmail.com>
2023-02-08 14:47:34 -0800

    Saved redux store over page loads

--- src/App.js
+++ src/App.js
@@ -1,31 +1,30 @@
 import React from "react";
 import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 
 import { Authentication } from "./features/auth/Authentication";
 import { configureStore } from "./store/Store";
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route
-} from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { Challenges } from "./features/challenges/Challenges";
 
-const store = configureStore();
+const { store, persistor } = configureStore();
 
 export default class App extends React.Component {
   render() {
     return (
       <Provider store={store}>
-        <Router>
-          <Switch>
-            <Route path="/challenges">
-              <Challenges />
-            </Route>
-            <Route path="">
-              <Authentication />
-            </Route>
-          </Switch>
-        </Router>
+        <PersistGate loading={null} persistor={persistor}>
+          <Router>
+            <Switch>
+              <Route path="/challenges">
+                <Challenges />
+              </Route>
+              <Route path="">
+                <Authentication />
+              </Route>
+            </Switch>
+          </Router>
+        </PersistGate>
       </Provider>
     );
   }
--- src/store/Store.js
+++ src/store/Store.js
@@ -1,12 +1,23 @@
 import { createStore, applyMiddleware, compose } from "redux";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 import thunkMiddleware from "redux-thunk";
 
 import { RootReducer } from "./RootReducer";
 
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, RootReducer);
+
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 export const configureStore = (initialState = {}) => {
-  const enhancer = compose(composeEnhancers( applyMiddleware(thunkMiddleware)));
+  const enhancer = compose(composeEnhancers(applyMiddleware(thunkMiddleware)));
 
-  return createStore(RootReducer, initialState, enhancer);
+  const store = createStore(persistedReducer, initialState, enhancer);
+  const persistor = persistStore(store);
+  return { store, persistor };
 };
