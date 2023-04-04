1a6b6f740b54de6c200b68aa0abc864369421d85
Paul <paulglujan@gmail.com>
2023-04-04 11:04:09 -0700

    Cleaning up integration test

--- src/__tests__/integration/index.spec.js
+++ src/__tests__/integration/index.spec.js
@@ -2,17 +2,19 @@ import { fireEvent, screen, waitFor } from "@testing-library/react";
 import { render } from "../../utility/testRender";
 import App from "../../App";
 
+const USERNAME = "testuser@joro.tech";
+const PASSWORD = "mockUserPassword";
+
 describe.only("Authentication page", () => {
-  it("renders", async () => {
-    // const { debug } = render(<App />);
+  it("Signs in and Joins Challenge", async () => {
     render(<App />);
     expect(screen.getByRole("heading")).toHaveTextContent("Sign In");
     const usernameInput = screen.getByTestId("username");
     fireEvent.change(usernameInput, {
-      target: { value: "testuser@joro.tech" },
+      target: { value: USERNAME },
     });
     const passwordInput = screen.getByTestId("password");
-    fireEvent.change(passwordInput, { target: { value: "mockUserPassword" } });
+    fireEvent.change(passwordInput, { target: { value: PASSWORD } });
     const button = screen.getByRole("button", { name: "Sign In" });
     fireEvent.click(button);
     await waitFor(() => {
@@ -31,12 +33,12 @@ describe.only("Authentication page", () => {
     const firstJoinButton = screen.getAllByRole("button", { name: "Join" })[0];
     fireEvent.click(firstJoinButton);
     await waitFor(() => {
-      expect(screen.getAllByRole("button", { name: "Loading" })).toHaveLength(1);
+      expect(screen.getAllByRole("button", { name: "Loading" })).toHaveLength(
+        1
+      );
     });
     await waitFor(() => {
       expect(screen.getAllByRole("button", { name: "Unjoin" })).toHaveLength(1);
     });
-    // debug();
-
   });
 });
